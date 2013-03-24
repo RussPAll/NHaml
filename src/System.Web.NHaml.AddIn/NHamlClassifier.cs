03584d72f31dc58cceac40e5e8607cd5dcbc5e59
Russell Allen <russell.homeserver@gmail.com>
2013-03-24 17:27:03 +0000

    Extended NHamlClassifier and added tests

--- src/System.Web.NHaml.AddIn/NHamlClassifier.cs
+++ src/System.Web.NHaml.AddIn/NHamlClassifier.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.NHaml.IO;
+using System.Web.NHaml.Parser;
+using System.Web.NHaml.Parser.Rules;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 
 namespace NHamlSyntaxHighlighter
 {
-    class NHamlClassifier : IClassifier
+    public class NHamlClassifier : IClassifier
     {
         readonly IClassificationTypeRegistryService _classificationRegistry;
         readonly ITextBuffer _buffer;
 
-        internal NHamlClassifier(ITextBuffer buffer, IClassificationTypeRegistryService classificationRegistry)
+        public NHamlClassifier(ITextBuffer buffer, IClassificationTypeRegistryService classificationRegistry)
         {
             _classificationRegistry = classificationRegistry;
             _buffer = buffer;
@@ -38,17 +41,60 @@ namespace NHamlSyntaxHighlighter
                               ? startLine
                               : span.Snapshot.GetLineFromPosition(span.End);
 
-            return new SnapshotSpan(startLine.Start, endLine.End);
+            return new SnapshotSpan(
+                startLine.Start,
+                endLine.End);
         }
 
         public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
         {
-            // And now parse the given paragraph and return classification spans for everything
-            var spans = new List<ClassificationSpan>
-                {
-                    new ClassificationSpan(span, _classificationRegistry.GetClassificationType("nhaml"))
-                };
+            ITextSnapshot snapshot = span.Snapshot;
+            string text = snapshot.GetText(span);
+
+            var document = new HamlTreeParser()
+                .ParseHamlFile(new HamlFileLexer().Read(text));
+
+            var spans = new List<ClassificationSpan>();
+            AddClassificationSpans(span, spans, document);
             return spans;
         }
+
+        private void AddClassificationSpans(SnapshotSpan snapshotSpan, List<ClassificationSpan> spans, HamlNode node)
+        {
+            if (node.SourceFileCharIndex + node.SourceFileCharCount > 0)
+            {
+                var span = new SnapshotSpan(snapshotSpan.Snapshot, node.SourceFileCharIndex, node.SourceFileCharCount);
+                var type = GetClassificationTypeForMarkdownToken(node.GetType());
+                spans.Add(new ClassificationSpan(span, type));
+            }
+            foreach (var childNode in node.Children)
+                AddClassificationSpans(snapshotSpan, spans, childNode);
+        }
+
+        static readonly Dictionary<Type, string> TokenToClassificationType = new Dictionary<Type, string>
+        {
+            { typeof(HamlNodeCode), "nhaml.code" },
+            { typeof(HamlNodeTag), "nhaml.tag" },
+            { typeof(HamlNodeTagId), "nhaml.tagId" },
+            { typeof(HamlNodeTagClass), "nhaml.tagClass" },
+            { typeof(HamlNodeDocType), "nhaml.docType" },
+            { typeof(HamlNodeHtmlComment), "nhaml.htmlComment" },
+            { typeof(HamlNodeHamlComment), "nhaml.hamlComment" },
+            { typeof(HamlNodeEval), "nhaml.eval" },
+            { typeof(HamlNodePartial), "nhaml.partial" },
+            { typeof(HamlNodeTextLiteral), "nhaml.textLiteral" },
+            { typeof(HamlNodeTextVariable), "nhaml.textVariable" },
+            { typeof(HamlNodeHtmlAttributeCollection), "nhaml.htmlAttributeCollection" },
+        };
+
+        IClassificationType GetClassificationTypeForMarkdownToken(Type tokenType)
+        {
+            string classificationType;
+            if (!TokenToClassificationType.TryGetValue(tokenType, out classificationType))
+                throw new ArgumentException("Unable to find classification type for " + tokenType, "tokenType");
+
+            return _classificationRegistry.GetClassificationType(classificationType);
+        }
+
     }
 }
---
+++ tests/System.Web.NHaml.AddIn.Tests/NHamlClassifier_IntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Moq;
+using NHamlSyntaxHighlighter;
+using NUnit.Framework;
+
+namespace System.Web.NHaml.AddIn.Tests
+{
+    public class NHamlClassifier_IntegrationTests
+    {
+        private NHamlClassifier _classifier;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var bufferMock = new Mock<ITextBuffer>();
+            var classificationTypeRegistryServiceMock = new Mock<IClassificationTypeRegistryService>();
+            _classifier = new NHamlClassifier(bufferMock.Object, classificationTypeRegistryServiceMock.Object);
+        }
+
+        [Test]
+        public void TBC()
+        {
+            // Arrange
+            var snapshotSpan = new SnapshotSpan();
+
+            // Act
+            var spans = _classifier.GetClassificationSpans(snapshotSpan);
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
